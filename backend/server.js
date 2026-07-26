2186fbcf28b2761baf910a8b91ecab5366ba3863
jatinkm007 <jatinkumishra693@gmail.com>
2026-07-26 23:38:07 +0530

    Vercel Deploy

--- backend/server.js
+++ backend/server.js
@@ -76,5 +76,5 @@ app.get('/posts', async (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}/users and http://localhost:${PORT}/posts`);
 });
